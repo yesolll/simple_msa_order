618fbe9c7b615dc28bec5248e0553262e9029ff7
9595kys <9595kys@gmail.com>
2022-05-13 02:10:45 +0900

    rest framwork setting
    views, urls

--- user_order/views.py
+++ user_order/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+class ShopViewSet(viewsets.ViewSet):
+    def list(self, request): # /api/shop
+        pass
+    
+    def retrieve(self, request): # /api/shop
+        pass
+    
+    def create(self, request, pk=None): # /api/shop/<str:idx>
+        pass
+    
+    def update(self, request, pk=None): # /api/shop/<str:idx>
+        pass
+    
+    def destroy(self, request, pk=None): # /api/shop/<str:idx>
+        pass
+
+class OrderViewSet(viewsets.ViewSet):
+    def list(self, request): # /api/order
+        pass
+
+    def retrieve(self, request): # /api/order
+        pass
+
+    def create(self, request, pk=None): # /api/order/<str:idx>
+        pass
+
+    def update(self, request, pk=None): # /api/order/<str:idx>
+        pass
+
+    def destroy(self, request, pk=None): # /api/order/<str:idx>
+        pass
+
