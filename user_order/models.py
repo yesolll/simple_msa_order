0bbb63dfb0e94aa132c4facde5f4f0c57ebcad06
9595kys <9595kys@gmail.com>
2022-05-13 02:02:09 +0900

    db migrate

--- user_order/models.py
+++ user_order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=20)
+    shop_address = models.CharField(max_length=50)
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    order_date = models.DateTimeField('date ordered')
+    delivery_address = models.CharField(max_length=50)
+    delivery_finish_yn = models.BooleanField(default=0)
